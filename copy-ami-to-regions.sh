79dcd876281fe1c06052950e6cd0e5d8bca4d8db
Vinay Nandipur Rudraiah <vinay.nandipur@gmail.com>
2020-01-17 14:29:51 +0530

    copy-ami-to-regions.sh to copy AMIs between regions

---
+++ copy-ami-to-regions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source="us-east-1"
+ami_name="test-ami-name"
+source_ami_id="ami-xxxxxxxxxxxxx"
+
+for i in `cat /Users/vrudraiah/Documents/scripts/destination-regions.txt` ;
+
+do
+
+printf "Source AMI:$source_ami_id getting copied from $source to $i"
+
+aws ec2 copy-image --source-image-id $source_ami_id --source-region $source --region $i --name $ami_name
+
+done
