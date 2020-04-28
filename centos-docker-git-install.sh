4dc7c230754bd0d3626a54016014cc2ca775fea5
Vinay <58210167+vinay-nandipur@users.noreply.github.com>
2020-04-28 14:26:31 +0530

    included 'yum update' as initial step as well

--- centos-docker-git-install.sh
+++ centos-docker-git-install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#update yum repos
+sudo yum update -y
+
 #remove docker old installs
 sudo yum remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine             
 
