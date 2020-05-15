30f49271ef9c18d078f6a6413c27714d51636b23
Vinay <58210167+vinay-nandipur@users.noreply.github.com>
2020-05-15 16:13:09 +0530

    updated usermod syntax

--- rhel8_dev_env_setup.sh
+++ rhel8_dev_env_setup.sh
@@ -33,7 +33,7 @@ sudo yum remove -y docker docker-client docker-client-latest docker-common docke
 sudo yum install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm -y
 sudo yum -y install docker-ce docker-ce-cli
 sudo systemctl start docker && sudo systemctl enable docker
-sudo usermod -aG docker $USER
+sudo usermod -a -G docker $USER
 newgrp docker
 
 #install docker-compose
